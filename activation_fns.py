fixed softmax gradient

activation_fns.py
@@ -15,7 +15,7 @@ class Softmax(ActivationFunc):
     @classmethod
     def activate(cls, z: np.ndarray) -> np.ndarray:
         """
-        z: n-dimensional vector
+        z:  n-dimensional vector
 
         Turns the elements of `z` into 'probabilities' by scaling `z` by 1/sum(`z`'s elements)
         """
@@ -25,13 +25,10 @@ class Softmax(ActivationFunc):
     @classmethod
     def deriv(cls, z: np.ndarray) -> np.ndarray:
         """
-        z: n-dimensional vector
+        z:  n-dimensional vector
         
         Applies the Jacobian of the softmax function to the vector `z`.
         """
-        z = z.reshape(-1)
-        return np.array([
-            [-z[i] * z[j] if i != j else z[i]*(1-z[j]) 
-             for i in range(z.size)] 
-            for j in range(z.size)
-        ])
+        z = Softmax.activate(z)
+        z = z.reshape((1,-1))
+        return np.diagflat(z) - z.T @ z

tests/test_activation_fns.py
@@ -11,7 +11,8 @@ def test_activate():
     assert np.sum(softZ) == 1
 
 def test_deriv():
-   n = 100
-   z = np.arange(n)
+   n = 4
+   np.random.seed(42)
+   z = np.random.rand(n)
    error = check_grad(Softmax.activate, Softmax.deriv, x0=z)
    assert error < 0.001